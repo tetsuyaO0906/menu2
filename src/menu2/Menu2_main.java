a9439f356bb4e8571c59f194ee8a1371e65f303d
50616008 <50616008@50616008-pc>
2018-01-15 17:55:07 +0900

    homework

---
+++ src/menu2/Menu2_main.java
@@ -0,0 +1,12 @@
+package menu2;
+
+public class Menu2_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Menu2_view menu = new Menu2_view();
+        menu.setBounds(5,5,700,550);
+        menu.setVisible(true);
+	}
+
+}
